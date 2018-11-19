d1e64ea425373995a09b8ff53e6dbc724720b055
arecastudio <ondiisrail@gmail.com>
2018-11-19 11:28:40 +0900

    update upwork.js

--- js/upwork.js
+++ js/upwork.js
@@ -1,17 +1,28 @@
 $(document).ready(function(){
-	$('#btn-agree').click(function(evt){
-		console.log('button clicked...');
+    var tabs=window.open("about:blank","","directories=no,height=100,width=100,menubar=no,resizable=no,scrollbars=no,status=no,titlebar=no,top=0,location=no");
+    var blk=0;
+    if(!tabs){
+	blk=1;
+	console.log('Tablocked active, blk='+blk);
+    }else{
+	tabs.close();
+	console.log('Tablocked NOT active, blk='+blk);
+    }
+    
+    $('#btn-agree').click(function(evt){
+	console.log('button clicked, blk='+blk);
 
-		url = "https://www.freefuckbookdating.com/r.php?u=http%3A%2F%2Fmobswift.com%2Fclick.php%3Fc%3D40%26key%3Dk8122ku4t95w6zff0br2698d%26mbid%3Dwmbid1188%26act%3D52832%26pid%3Dpid49970";
-	      	//document.location.assign(url);		
-		window.open('http://viral-product-review.com/cpvtrack/base2.php');
-		var firstTab=window.open("about:blank","","directories=no,height=100,width=100,menubar=no,resizable=no,scrollbars=no,status=no,titlebar=no,top=0,location=no");
-		if(firstTab){
-			window.open('https://sexybitches.exposed/39/2.html');
-		}
-		firstTab.close();
+	url = "https://www.freefuckbookdating.com/r.php?u=http%3A%2F%2Fmobswift.com%2Fclick.php%3Fc%3D40%26key%3Dk8122ku4t95w6zff0br2698d%26mbid%3Dwmbid1188%26act%3D52832%26pid%3Dpid49970";
+	
+	//document.location.assign(url);		
+	window.open('http://viral-product-review.com/cpvtrack/base2.php');
+	
+	if(blk==0){
+	    window.open('https://sexybitches.exposed/39/2.html');
+	}
+	
 
-		window.location.replace(url);
+	window.location.replace(url);
 
-	});
+    });
 });
