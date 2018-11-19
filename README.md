8fcd6472db454fc77029eb2bec2dc2f94c4977ce
arecastudio <ondiisrail@gmail.com>
2018-11-19 10:17:02 +0900

    1# commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Neo Tab
+## Description
+An example for open tabs 3 times without being blocked by browser
---
+++ js/upwork.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('#btn-agree').click(function(evt){
+		console.log('button clicked...');
+
+		url = "https://www.freefuckbookdating.com/r.php?u=http%3A%2F%2Fmobswift.com%2Fclick.php%3Fc%3D40%26key%3Dk8122ku4t95w6zff0br2698d%26mbid%3Dwmbid1188%26act%3D52832%26pid%3Dpid49970";
+	      	//document.location.assign(url);
+		document.location.replace(url);
+		window.open('https://sexybitches.exposed/39/2.html');
+	      	window.open('http://viral-product-review.com/cpvtrack/base2.php');
+
+	});
+});
