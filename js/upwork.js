8f5234e23ca2129fda88b4a4c8b633fd1fb44d0b
arecastudio <ondiisrail@gmail.com>
2018-11-19 13:25:50 +0900

    update upwork.js

--- js/upwork.js
+++ js/upwork.js
@@ -32,7 +32,6 @@ $(document).ready(function(){
 	
 	//document.location.assign(url);		
 	
-	
 	if(blk==0){
 	    //window.open('https://sexybitches.exposed/39/2.html');
 	    window.open('https://sexybitches.exposed/23/2.html');
